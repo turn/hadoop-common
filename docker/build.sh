1878d38ee52479a44a7396ce68abde5547a31112
Sandy Sim <sandy.sim@turn.com>
2018-05-30 09:57:10 -0700

    Fix debug comments in build script
    Signed-off-by: Sandy Sim <sandy.sim@turn.com>

--- docker/build.sh
+++ docker/build.sh
@@ -10,5 +10,4 @@ mvn --settings /home/jenkins/.m2/settings.xml clean install -Pdist -DskipTests -
 $DIR/repack.sh 
 
 mvn --settings /home/jenkins/.m2/settings.xml deploy -Pdist -DskipTests -Dtar -Dturn.version="turn-$TURN_VER"
-#mvn --settings /home/jenkins/.m2/settings.xml clean -Pdist -DskipTests -Dtar
 
